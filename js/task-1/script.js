95f5ababd26324394371fded95f7c20f47e872a9
Nik Kolesnikov <niknills94@gmail.com>
2022-08-15 14:51:47 +0300

    feat: added task 2 solution

--- js/task-1/script.js
+++ js/task-1/script.js
@@ -3,3 +3,9 @@
 const enterText = prompt("Please, enter text!");
 
 console.log(enterText);
+
+const enterNumberA = +prompt("Please, enter number A"),
+  enterNumberB = +prompt("Please, enter number B"),
+  enterNumberC = +prompt("Please, enter number C");
+
+alert(enterNumberA + enterNumberB + enterNumberC);
