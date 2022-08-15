f45fbca853a4033610e741f91e83412037fe918d
Nik Kolesnikov <niknills94@gmail.com>
2022-08-15 14:53:23 +0300

    feat: added task 3 solution

--- js/task-1/script.js
+++ js/task-1/script.js
@@ -9,3 +9,12 @@ const enterNumberA = +prompt("Please, enter number A"),
   enterNumberC = +prompt("Please, enter number C");
 
 alert(enterNumberA + enterNumberB + enterNumberC);
+
+const enterYourName = prompt("Please, enter your name"),
+  enterYourAge = +prompt("Please, enter your age");
+
+if (enterYourAge >= 18) {
+  alert(`Welcome, ${enterYourName}`);
+} else {
+  alert(`Sorry, ${enterYourName}, access denied`);
+}
